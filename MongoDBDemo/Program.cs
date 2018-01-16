878c462b918275a432b85baecec13772935bb703
David Andersson <David Andersson>
2018-01-16 09:55:19 +0100

    Add project files.

---
+++ MongoDBDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace MongoDBDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ReadLine();
+			MongoClient client = new MongoClient();// "mongodb://localhost:27017");
+			var db = client.GetDatabase("forestdb");
+			var collection = db.GetCollection<BsonDocument>("animals");
+
+			long count = collection.Count(new BsonDocument());
+			Console.WriteLine("The database has " + count + " documents");
+			Console.ReadLine();
+		}
+	}
+}
